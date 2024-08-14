d51c4941f248c0edab7d5ed6173c7e76cd819b3c
DanielSantosProg <danielsantos_lol@hotmail.com>
2024-08-14 11:42:02 -0300

    Created login page, Added navigation

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import Login from "./views/Login";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  Stack = createNativeStackNavigator();
+  const [loaded, error] = useFonts({
+    "Kameron-Regular": require("./assets/fonts/Kameron-Regular.ttf"),
+    "Kameron-Bold": require("./assets/fonts/Kameron-Bold.ttf"),
+    "Kameron-SemiBold": require("./assets/fonts/Kameron-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "Login" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ views/Login.js
@@ -0,0 +1,116 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.textoBold, styles.logo]}>PAPACAPIM</Text>
+      <Text style={[styles.texto, styles.header]}>Entre no Papacapim</Text>
+      <View style={styles.inputs}>
+        <Text style={styles.texto}>Usuário</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite o nome do usuário"
+          placeholderTextColor="gray"
+        />
+        <Text style={styles.texto}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Digite a sua senha"
+          placeholderTextColor="gray"
+        />
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={[styles.touchable, styles.loginBut]}>
+          <Text style={styles.textoBold}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.touchable, styles.forgotPassword]}>
+          <Text style={[styles.textoBold, styles.forgotText]}>
+            Esqueceu a Senha?
+          </Text>
+        </TouchableOpacity>
+        <Text style={[styles.texto, styles.noAccount]}>
+          Não tem uma conta? <Text style={styles.textoSpan}>Cadastre-se</Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#222831",
+  },
+  textoBold: {
+    color: "#EEE",
+    fontFamily: "Kameron-Bold",
+    fontSize: 24,
+  },
+  texto: {
+    color: "#EEE",
+    fontFamily: "Kameron-Regular",
+    fontSize: 20,
+  },
+  textoSpan: {
+    color: "#76ABAE",
+  },
+  logo: {
+    bottom: 170,
+  },
+  header: {
+    bottom: 80,
+    fontFamily: "Kameron-SemiBold",
+  },
+  input: {
+    marginTop: 5,
+    marginBottom: 15,
+    borderColor: "#FFF",
+    borderWidth: 0.3,
+    borderRadius: 4,
+    width: 300,
+    height: 50,
+    backgroundColor: "#31363F",
+  },
+  inputs: {
+    bottom: 30,
+  },
+  touchable: {
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 300,
+    height: 50,
+  },
+  buttons: {
+    top: 25,
+  },
+  loginBut: {
+    backgroundColor: "#76ABAE",
+    borderRadius: 30,
+  },
+  forgotPassword: {
+    borderColor: "#EEE",
+    borderWidth: 0.3,
+    borderRadius: 30,
+  },
+  forgotText: {
+    fontSize: 20,
+  },
+  noAccount: {
+    top: 15,
+    fontSize: 16,
+    alignSelf: "center",
+  },
+});
+
+export default Login;
